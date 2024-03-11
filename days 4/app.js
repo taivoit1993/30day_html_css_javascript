dd5d7af33384ea785d3f6127f55ef39dfa258fec
taivoit1993 <taivo.mile@gmail.com>
2024-03-11 23:34:23 +0700

    days 4update javascript

--- days 4/app.js
+++ days 4/app.js
@@ -1,8 +1,63 @@
-$(document).ready(function(){
-    $(".item img").on("mouseenter", function(e){
+$(document).ready(function () {
+    var currentIndex = 0;
+    var len = $(".item img").length;
+
+    function setGalleryImage(src) {
+        $(".gallery__img img").fadeOut(200, function(){
+            $(this).attr("src",src);
+            $(this).fadeIn(500);
+        });
+    }
+
+    function toggleGallery(show){
+        if(show == true){
+            $(".gallery").addClass("show");
+        }else{
+            $(".gallery").removeClass("show");
+        }
+    }
+
+    function buttonGalleryImage(index){
+       if(index <= 0){
+        $(".gallery__prev").addClass("btn-hide");
+       }
+       else{
+        $(".gallery__prev").removeClass("btn-hide");
+       }
+
+       if(index >= len -1){
+        $(".gallery__next").addClass("btn-hide");
+       }else{
+        $(".gallery__next").removeClass("btn-hide");
+       }
+    }
+
+    $(".item img").on("mouseenter", function (e) {
         $(this).addClass("img-zoo");
+    }).on("mouseleave", function (e) {
+        $(this).removeClass("img-zoo");
+    }).on("click", function (e) {
+        currentIndex = $(".item img").index(this);
+        toggleGallery(true);
+        setGalleryImage($(this).attr("src"));
     });
-    $(".item img").on("mouseleave", function(e){
-        $(this).removeClass("img-zoo",{duration: 50000});
+
+    $(".close").on("click", function () {
+        toggleGallery(false);
+    });
+
+    $(".gallery__next").on("click", function () {
+        currentIndex++;
+       
+        var src = $(".item img").eq(currentIndex).attr("src");
+        setGalleryImage(src);
+        buttonGalleryImage(currentIndex);
+    });
+
+    $(".gallery__prev").on("click", function () {
+        currentIndex--;
+        var src = $(".item img").eq(currentIndex).attr("src");
+        setGalleryImage(src);
+        buttonGalleryImage(currentIndex);
     });
 });
